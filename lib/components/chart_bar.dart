46f7bffc3a24a2fdc84a29d330ecfd699647179d
MarcosPalhao <75809289+MarcosPalhao@users.noreply.github.com>
2022-08-02 16:45:08 -0300

    feat: misc

--- lib/components/chart_bar.dart
+++ lib/components/chart_bar.dart
@@ -7,7 +7,7 @@ class ChartBar extends StatelessWidget {
   final double? value;
   final double? percentage;
 
-  ChartBar({
+  const ChartBar({
     this.label,
     this.value,
     this.percentage,
